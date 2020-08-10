b3d350ad2fd6be5f3960959716870cab86431fec
Amer Toukan <amertoukan@amertoukan.hitronhub.home>
2020-08-10 18:47:45 -0400

    Splash complete"

--- components/Auth/Splash.js
+++ components/Auth/Splash.js
@@ -8,5 +8,59 @@ import { connect } from 'react-redux';
 import {initializeApp} from './login_actions'; 
 
 class Splash extends Component {
-    
+    constructor(props){
+        super(props); 
+        this.state = {}
+    }
+
+    componentWillMount() {
+        setTimeout(() => this.props.initializeApp(), 2000)
+    }
+
+    componentWillReceiveProps(nextProps){
+        if (nextProps.authenticated) this.props.navigation.navigate('WeLoggedIn')
+    }
+
+    render(){
+        const { container, image, text } = styles 
+
+        return (
+            <View style = {container}> 
+                <Image 
+                style = {image}
+                source = {require('./logo.png')}
+                />
+
+                <Animatable.Text
+                style={text}
+                duration = {1500}
+                animation="rubberBand"
+                easing="linear"
+                iterationCount="infinite"
+                >
+                    Loading...
+                </Animatable.Text>
+
+                <Text>{(this.props.authenticated) ? 'LOGGED IN' : 'NOT LOGGED IN'}</Text>
+            </View>
+        )
+    }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        backgroundColor: "#F0F0F0"
+    }, 
+    image: {
+        height: 110, 
+        resizeMode: "contain", 
+    },
+    text: {
+        marginTop: 50, 
+        fontSize: 15, 
+        color: "#1A1A1A"
+    }
+})
